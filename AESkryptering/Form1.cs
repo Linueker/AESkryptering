33d82ff956b496a05b46556262e9dfa9355a9a9b
timchristoffer <timdaahl@gmail.com>
2024-08-15 14:50:04 +0200

    Tims DB

--- AESkryptering/Form1.cs
+++ AESkryptering/Form1.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
 namespace AESkryptering
 {
     public partial class Form1 : Form
@@ -13,8 +17,10 @@ namespace AESkryptering
         private void button1_Click(object sender, EventArgs e)
         {
             encrypted = Encrypt.EncryptString(Text.Text.ToString(), Password.Text.ToString());
-            Text.Text = encrypted;
-            Password.Text = "";
+            textBox2.Text = encrypted;
+
+            // Spara lösenord och krypterad sträng i databasen
+            SaveToDatabase(password, encrypted);
         }
 
         private void Text_TextChanged(object sender, EventArgs e)
@@ -38,6 +44,25 @@ namespace AESkryptering
             textBox2.Text = decrypted;
 
         }
-        
+
+        private void SaveToDatabase(string password, string encryptedMessage)
+        {
+            string connectionString = "Data Source=TIM;Initial Catalog=PasswordDB;Integrated Security=True";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "INSERT INTO Passwords (Password, Message) VALUES (@Password, @Message)";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Password", password);
+                    command.Parameters.AddWithValue("@Message", encryptedMessage);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
     }
 }
